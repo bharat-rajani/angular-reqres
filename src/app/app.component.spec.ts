d667c8ae2a8152d3af4eeeef30a5adc49fb88135
bharat-rajani <bharat.ramrajani@gmail.com>
2019-03-23 23:43:58 +0530

    renamed the application name in json configs.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'newApp'`, () => {
+  it(`should have as title 'angular-reqres'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('newApp');
+    expect(app.title).toEqual('angular-reqres');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to newApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-reqres!');
   });
 });
